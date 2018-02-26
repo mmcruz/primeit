3862a67b4c9136a4ca9eee0fe48bcd6473cb6e63
Caiuby Freitas <caiubyfreitas@users.noreply.github.com>
2018-02-26 16:01:30 +0000

    1.0.0.1
    Files sync

---
+++ PrimeIT/src/PrimeIT/global/Test.java
@@ -0,0 +1,10 @@
+import PrimeIT.Database;
+
+public class Test {
+    public static void main(String args[]) {
+		DBCommand cmd = new DBCommand();
+		
+        System.out.println("Sum of x+y = " + z);
+    }
+}
+
